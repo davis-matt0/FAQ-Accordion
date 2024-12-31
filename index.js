3560c8c9d6b94e496583aa41011f9500df3fb116
Matt <154559729+davis-matt0@users.noreply.github.com>
2024-12-31 08:16:02 -0500

    fix javascript functionality

--- index.js
+++ index.js
@@ -1,59 +1,25 @@
-const answers = document.querySelectorAll(".answer");
-const btns = document.querySelectorAll(".btn");
 const questions = document.querySelectorAll(".question-container");
+const answers = document.querySelectorAll(".answer");
 
-// for (let answer = 0; answer < answers.length; answer++) {
-//   answers[answer].classList.add("hidden");
-//   for (let btn = 0; btn < btns.length; btn++) {
-//     btns[btn].addEventListener("click", function () {
-//       answers[answer].classList.toggle("hidden");
-//       answers[answer].classList.toggle("visible");
-//       if (answers[answer].classList.contains("visible")) {
-//         btns[btn].innerHTML = `
-//       <img
-//             src="./assets/images/icon-minus.svg"
-//             alt="a minus icon that hides the answer to this question"
-//             class="plus-icon"
-//         />
-//       `;
-//       } else {
-//         btns[btn].innerHTML = `
-//         <img
-//             src="./assets/images/icon-plus.svg"
-//             alt="a plus icon that reveals the answer to this question"
-//             class="plus-icon"
-//         />
-//         `;
-//       }
-//     });
-//   }
-// }
+answers.forEach((answer) => {
+  answer.classList.add("hidden");
+});
 
-for (let answer = 0; answer < answers.length; answer++) {
-  answers[answer].classList.add("hidden");
-  for (let question = 0; question < questions.length; question++) {
-    questions[question].addEventListener("click", function () {
-      answers[answer].classList.toggle("hidden");
-      answers[answer].classList.toggle("visible");
-    });
-    for (let btn = 0; btn < btns.length; btn++) {
-      if (answers[answer].classList.contains("visible")) {
-        btns[btn].innerHTML = `
-      <img
-            src="./assets/images/icon-minus.svg"
-            alt="a minus icon that hides the answer to this question"
-            class="plus-icon"
-        />
-      `;
-      } else {
-        btns[btn].innerHTML = `
-        <img
-            src="./assets/images/icon-plus.svg"
-            alt="a plus icon that reveals the answer to this question"
-            class="plus-icon"
-        />
-        `;
-      }
+questions.forEach((q) => {
+  q.addEventListener("click", function () {
+    let container = this.parentElement;
+    let answer = container.querySelector(".answer");
+    let icon = container.querySelector(".plus-icon");
+    answer.classList.toggle("hidden");
+    answer.classList.toggle("visible");
+    if (answer.classList.contains("visible")) {
+      icon.src = "./assets/images/icon-minus.svg";
+      icon.alt =
+        "a minus icon that hides the answer to the question when clicked";
+    } else {
+      icon.src = "./assets/images/icon-plus.svg";
+      icon.alt =
+        "a plus icon that reveals the answer to the question when clicked";
     }
-  }
-}
+  });
+});
